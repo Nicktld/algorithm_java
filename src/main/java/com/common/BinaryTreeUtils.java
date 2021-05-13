a80d9cbc015e847149c7080f6297ccfc7cee2f5c
Icemamal <ltang2016a@gmail.com>
2021-05-14 06:40:54 +0800

    Add DFSTree && Modify BinaryTree Utils

--- src/main/java/com/common/BinaryTreeUtils.java
+++ src/main/java/com/common/BinaryTreeUtils.java
@@ -11,6 +11,9 @@ public class BinaryTreeUtils {
 
     public static final Logger LOGGER = LogManager.getLogger(BinaryTreeUtils.class);
 
+    /*
+    297. Serialize and Deserialize Binary Tree
+     */
     public static <E> BinaryTree<E> buildTree(E[] arr) {
         if(arr != null || arr.length == 0) {
             return null;
@@ -39,6 +42,9 @@ public class BinaryTreeUtils {
         return root;
     }
 
+    /*
+    144. Binary Tree Preorder Traversal
+     */
     public static <E> List<E> preorder(BinaryTree<E> root) {
         List<E> travList = new ArrayList<>();
         if(root == null) return travList;
@@ -54,6 +60,9 @@ public class BinaryTreeUtils {
         return travList;
     }
 
+    /*
+    94. Binary Tree Inorder Traversal
+     */
     public static <E> List<E> inorder(BinaryTree<E> root) {
         List<E> travList = new ArrayList<>();
         if(root == null) return travList;
@@ -71,6 +80,9 @@ public class BinaryTreeUtils {
         return travList;
     }
 
+    /*
+    145. Binary Tree Postorder Traversal
+     */
     public static <E> List<E> postorder(BinaryTree<E> root) {
         List<E> travList = new LinkedList<>();
         if(root == null) return travList;
@@ -85,5 +97,39 @@ public class BinaryTreeUtils {
             if(node.right != null) stack.push(node.right);
         }
         return travList;
+    }
+
+    /*
+    104. Maximum Depth of Binary Tree - DFS Preferred
+     */
+    public static <E> int maxDepth(BinaryTree<E> root) {
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return (int) Math.max(left, right) + 1;
+    }
+
+    /*
+    111. Minimum Depth of Binary Tree - BFS Preferred
+     */
+    public static <E> int minDepth(BinaryTree<E> root) {
+        if(root == null) return 0;
+
+        Deque<BinaryTree<E>> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int level = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                BinaryTree<E> node = queue.poll();
+                if (node.left == null && node.right == null) return level;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            level++;
+        }
+        return level;
     }
 }
---
+++ src/main/java/com/dfs/DFSTree.java
@@ -0,0 +1,71 @@
+package com.dfs;
+
+import com.POJO.BinaryTree;
+import com.common.BinaryTreeUtils;
+import com.twoPointers.TPNodes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFSTree {
+    public static final Logger LOGGER = LogManager.getLogger(DFSTree.class);
+
+    /*
+    110. Balanced Binary Tree
+     */
+    public static <E> boolean isBalanced(BinaryTree<E> root) {
+        if(root == null) return true;
+
+        int left = BinaryTreeUtils.maxDepth(root.left);
+        int right = BinaryTreeUtils.maxDepth(root.right);
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(left - right) <= 1;
+    }
+
+    public static <E> boolean isBalancedII(BinaryTree<E> root) {
+        return getHeight(root) != -1;
+    }
+    private static <E> int getHeight(BinaryTree<E> root) {
+        if(root == null) return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        return Math.max(left, right) + 1;
+    }
+
+    /*
+    545. Boundary of Binary Tree - Elegant Recursion Usage
+     */
+    public static <E> List<E> boundaryofBinaryTree(BinaryTree<E> root) {
+        List<E> boundaryList = new ArrayList<>();
+        if(root == null) return boundaryList;
+
+        boundaryList.add(root.val);
+        getLeftBoundary(root.left, boundaryList);
+        getLeaves(root.left, boundaryList);
+        getLeaves(root.right, boundaryList);
+        getRightBoundary(root.right, boundaryList);
+        return boundaryList;
+    }
+    private static <E> void getLeftBoundary(BinaryTree<E> node, List<E> list) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) return;
+        list.add(node.val);
+        if(node.left == null) getLeftBoundary(node.right, list);
+        else getLeftBoundary(node.left, list);
+    }
+    private static <E> void getRightBoundary(BinaryTree<E> node, List<E> list) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) return;
+        if(node.right == null) getRightBoundary(node.left, list);
+        else getRightBoundary(node.right, list);
+        list.add(node.val);
+    }
+    private static <E> void getLeaves(BinaryTree<E> node, List<E> list) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) list.add(node.val);
+        getLeaves(node.left,list);
+        getLeaves(node.left,list);
+    }
+}
